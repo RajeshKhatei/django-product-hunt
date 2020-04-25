2d1f96142ef89e9672221c05d6fa5478059b0f81
Rajesh Kumar Khatei <rajeshkukhatei@gmail.com>
2020-04-25 17:49:21 +0530

    product detail page and vote for product for logged in user

--- products/models.py
+++ products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.TextField()
+    publish_date = models.DateTimeField()
+    votes_total = models.IntegerField(default=1)
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    description = models.TextField()
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.description[:100]
+
+    def publish_date_format(self):
+        return self.publish_date.strftime('%b %e %Y')
